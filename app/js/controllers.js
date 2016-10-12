afc583bdfd821d3c518723885ead909851127461
Fernando R. Cangussu <frcangussu@gmail.com>
2016-10-12 06:47:21 -0300

    Adicionando código fonte do creator ionic

---
+++ app/js/controllers.js
@@ -0,0 +1,178 @@
+angular.module('app.controllers', [])
+  
+.controller('paginaEmBrancoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('tipoDeVisitaCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('alterarMeusDadosCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('cadastroDeDependentesCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('alteraODeDependentesCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('dependentesCadastradosCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('cadastroDeInquilinoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('cadastrarCondominioCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('cDigoDeLiberaOCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('liberaOEnviadaCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('criarEventoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('ticketsRecebidosCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('registrarVisitaCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('pessoalCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('cancelarConviteCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('profissionalCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('page23Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('infoConviteCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('validarPorteirosCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('trocarSNdicoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+      
+.controller('side-menu21Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('signupCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ app/js/routes.js
@@ -0,0 +1,181 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('paginaEmBranco', {
+    url: '/branco',
+    templateUrl: 'templates/paginaEmBranco.html',
+    controller: 'paginaEmBrancoCtrl'
+  })
+
+  .state('tabsController.tipoDeVisita', {
+    url: '/tipoVisita',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/tipoDeVisita.html',
+        controller: 'tipoDeVisitaCtrl'
+      }
+    }
+  })
+
+  .state('alterarMeusDados', {
+    url: '/alterarPerfil',
+    templateUrl: 'templates/alterarMeusDados.html',
+    controller: 'alterarMeusDadosCtrl'
+  })
+
+  .state('cadastroDeDependentes', {
+    url: '/cadDependentes',
+    templateUrl: 'templates/cadastroDeDependentes.html',
+    controller: 'cadastroDeDependentesCtrl'
+  })
+
+  .state('alteraODeDependentes', {
+    url: '/alteraDependente',
+    templateUrl: 'templates/alteraODeDependentes.html',
+    controller: 'alteraODeDependentesCtrl'
+  })
+
+  .state('dependentesCadastrados', {
+    url: '/page12',
+    templateUrl: 'templates/dependentesCadastrados.html',
+    controller: 'dependentesCadastradosCtrl'
+  })
+
+  .state('cadastroDeInquilino', {
+    url: '/cadInquilino',
+    templateUrl: 'templates/cadastroDeInquilino.html',
+    controller: 'cadastroDeInquilinoCtrl'
+  })
+
+  .state('cadastrarCondominio', {
+    url: '/cadCondominio',
+    templateUrl: 'templates/cadastrarCondominio.html',
+    controller: 'cadastrarCondominioCtrl'
+  })
+
+  .state('tabsController.cDigoDeLiberaO', {
+    url: '/qrcode',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/cDigoDeLiberaO.html',
+        controller: 'cDigoDeLiberaOCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.liberaOEnviada', {
+    url: '/sucesso-liberacao',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/liberaOEnviada.html',
+        controller: 'liberaOEnviadaCtrl'
+      }
+    }
+  })
+
+  .state('criarEvento', {
+    url: '/criar-evento',
+    templateUrl: 'templates/criarEvento.html',
+    controller: 'criarEventoCtrl'
+  })
+
+  .state('tabsController.ticketsRecebidos', {
+    url: '/visitante',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/ticketsRecebidos.html',
+        controller: 'ticketsRecebidosCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.registrarVisita', {
+    url: '/registrarVisita',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/registrarVisita.html',
+        controller: 'registrarVisitaCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.pessoal', {
+    url: '/visitaPessoal',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/pessoal.html',
+        controller: 'pessoalCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.cancelarConvite', {
+    url: '/cancelarConvite',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/cancelarConvite.html',
+        controller: 'cancelarConviteCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.profissional', {
+    url: '/visitaProfissional',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/profissional.html',
+        controller: 'profissionalCtrl'
+      }
+    }
+  })
+
+  .state('page23', {
+    url: '/compartilhar',
+    templateUrl: 'templates/page23.html',
+    controller: 'page23Ctrl'
+  })
+
+  .state('infoConvite', {
+    url: '/infoConvite',
+    templateUrl: 'templates/infoConvite.html',
+    controller: 'infoConviteCtrl'
+  })
+
+  .state('validarPorteiros', {
+    url: '/validarPortaria',
+    templateUrl: 'templates/validarPorteiros.html',
+    controller: 'validarPorteirosCtrl'
+  })
+
+  .state('trocarSNdico', {
+    url: '/page15',
+    templateUrl: 'templates/trocarSNdico.html',
+    controller: 'trocarSNdicoCtrl'
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('signup', {
+    url: '/page20',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+$urlRouterProvider.otherwise('/page1/registrarVisita')
+
+  
+
+});
